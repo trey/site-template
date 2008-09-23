06f50de16b19e46e0deaf3487b014b9e54a51638
Trey Piepmeier <trey@treypiepmeier.com>
2008-09-22 23:36:58 -0500

    Updated to ToggleVal 2.0.

--- js/jquery.toggleval.js
+++ js/jquery.toggleval.js
@@ -1,27 +1,60 @@
 /* -------------------------------------------------- *
- * ToggleVal Plugin for jQuery                        *
- * Version 1.0                                        *
+ * ToggleVal 2.0
+ * Updated: 9/15/08
  * -------------------------------------------------- *
- * Author:   Aaron Kuzemchak                          *
- * URL:      http://kuzemchak.net/                    *
- * E-mail:   afkuzemchak@gmail.com                    *
- * Date:     8/18/2007                                *
- * -------------------------------------------------- */
+ * Author: Aaron Kuzemchak
+ * URL: http://aaronkuzemchak.com/
+ * Copyright: 2008 Aaron Kuzemchak
+ * License: MIT License
+** -------------------------------------------------- */
 
-jQuery.fn.toggleVal = function(focusClass) {
-	this.each(function() {
-		$(this).focus(function() {
-			// clear value if current value is the default
-			if($(this).val() == this.defaultValue) { $(this).val(""); }
+(function($) {
+	$.fn.toggleVal = function(theOptions) {
+		theOptions = $.extend({
+			focusClass: "tv-focused", // class during focus
+			changedClass: "tv-changed", // class after focus
+			populateFrom: "default", // choose from: default, label, or alt
+			removeLabels: false // remove labels associated with the fields
+		}, theOptions);
+		
+		return this.each(function() {
+			// define our variables
+			var defText = "";
 			
-			// if focusClass is set, add the class
-			if(focusClass) { $(this).addClass(focusClass); }
-		}).blur(function() {
-			// restore to the default value if current value is empty
-			if($(this).val() == "") { $(this).val(this.defaultValue); }
+			// let's populate the text, if not default
+			switch(theOptions.populateFrom) {
+				case "alt":
+					defText = $(this).attr("alt");
+					$(this).val(defText);
+					break
+				case "label":
+					defText = $("label[for='" + $(this).attr("id") + "']").text();
+					$(this).val(defText);
+					break
+				default:
+					defText = $(this).val();
+			}
 			
-			// if focusClass is set, remove class
-			if(focusClass) { $(this).removeClass(focusClass); }
+			// let's give this field a special class, so we can identify it later
+			$(this).addClass("toggleval");
+			
+			// now that fields are populated, let's remove the labels if applicable
+			if(theOptions.removeLabels == true) { $("label[for='" + $(this).attr("id") + "']").remove(); }
+			
+			// on to the good stuff... the focus and blur actions
+			$(this).focus(function() {
+				if($(this).val() == defText) { $(this).val(""); }
+				
+				// add the focusClass, remove changedClass
+				$(this).addClass(theOptions.focusClass).removeClass(theOptions.changedClass);
+			}).blur(function() {
+				if($(this).val() == "") { $(this).val(defText); }
+				
+				// remove focusClass, add changedClass if, well, different
+				$(this).removeClass(theOptions.focusClass);
+				if($(this).val() != defText) { $(this).addClass(theOptions.changedClass); }
+					else { $(this).removeClass(theOptions.changedClass); }
+			});
 		});
-	});
-}
+	};
+})(jQuery);

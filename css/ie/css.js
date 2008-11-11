a6ee5a3ab6a0c2d4019a205eee38036b32264a42
Trey Piepmeier <trey@treypiepmeier.com>
2008-11-11 17:42:57 -0600

    Using jQuery to fix CSS in IE.

---
+++ css/ie/css.js
@@ -0,0 +1,19 @@
+/*
+Use this file to add enhanced CSS support to IE (or any browser).
+
+An example:
+
+	$('#nav li:first a').css('border', 'none');
+
+Then in your normal stylesheet, you could just do:
+
+	#nav li:first a { border: none; }
+
+Include it in your HTML like so:
+
+	<script src="/css/ie/css.js" type="text/javascript"></script>
+*/
+
+$(function() {
+    // Stuff to do as soon as the DOM is ready;
+});

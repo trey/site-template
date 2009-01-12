f7515374fbc22c73e058c0f788f7f3bae3a3d6e4
Trey Piepmeier <trey@treypiepmeier.com>
2009-01-12 00:18:33 -0600

    HTML 5 is good for you.

---
+++ css/ie/html5.js
@@ -0,0 +1,7 @@
+// http://orderedlist.com/articles/structural-tags-in-html5
+document.createElement('header');
+document.createElement('footer');
+document.createElement('section');
+document.createElement('aside');
+document.createElement('nav');
+document.createElement('article');

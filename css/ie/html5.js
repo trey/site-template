be64ee46c281eef20edd96cb0a2f4d9d6acf64e8
Trey Piepmeier <trey@treypiepmeier.com>
2009-10-28 14:17:00 -0500

    Updated things based on http://diveintohtml5.org/semantics.html

--- css/ie/html5.js
+++ css/ie/html5.js
@@ -1,7 +1,2 @@
-// http://orderedlist.com/articles/structural-tags-in-html5
-document.createElement('header');
-document.createElement('footer');
-document.createElement('section');
-document.createElement('aside');
-document.createElement('nav');
-document.createElement('article');
+// For discussion and comments, see: http://remysharp.com/2009/01/07/html5-enabling-script/
+(function(){if(!/*@cc_on!@*/0)return;var e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()

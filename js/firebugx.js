4c8ddf3714dba3cfd30366de35b6d0752d9d8728
Trey Piepmeier <trey@treypiepmeier.com>
2010-09-08 14:51:14 -0500

    Added firebugx.js for safe keeping.
    But I'm not sure if I'll use it.
    http://forrst.com/posts/firebugx_js-XBr

---
+++ js/firebugx.js
@@ -0,0 +1,10 @@
+// http://fbug.googlecode.com/svn/branches/firebug1.5/lite/firebugx.js
+if (!window.console || !console.firebug)
+{
+	var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+	"group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+
+	window.console = {};
+	for (var i = 0; i < names.length; ++i)
+		window.console[names[i]] = function() {}
+}
